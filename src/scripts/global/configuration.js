b35da6704f20ea8557001a9fa03d873eafcff954
sultanroomcode <sultanroomcode@gmail.com>
2021-06-14 23:07:30 +0700

    add configuration

---
+++ src/scripts/global/configuration.js
@@ -0,0 +1,11 @@
+const CONFIGURATION = {
+  KEY: 1,
+  BASE_URL: "https://restaurant-api.dicoding.dev/",
+  BASE_URL_IMAGE: "https://restaurant-api.dicoding.dev/images",
+  CACHE_NAME: "RestaurantMama-v1",
+  DATABASE_NAME: "restaurant-mama-db",
+  DATABASE_VERSION: 1,
+  OBJECT_STORE_ID: "restaurant",
+};
+
+export default CONFIGURATION;
