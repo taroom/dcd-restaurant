422939855e09c421703d774c28ac92e1bfe68ee2
sultanroomcode <sultanroomcode@gmail.com>
2021-06-16 17:03:11 +0700

    implement api handler

--- src/scripts/views/page/detail.js
+++ src/scripts/views/page/detail.js
@@ -1,9 +1,15 @@
+import RestaurantApiHandler from "../../data/restaurant-api-handler";
+import UrlParser from "../../routes/url-parser";
+
 const Detail = {
   async render() {
     return `<h1>Halaman Detail</h1>`;
   },
   async afterRender() {
     // fungsi after render
+    const url = UrlParser.parseActiveUrlWithoutCombiner();
+    const restaurantDetail = await RestaurantApiHandler.detail(url.id);
+    console.log(restaurantDetail);
   },
 };
 
--- src/scripts/views/page/home.js
+++ src/scripts/views/page/home.js
@@ -1,9 +1,13 @@
+import RestaurantApiHandler from "../../data/restaurant-api-handler";
+
 const Home = {
   async render() {
     return `<h1>Halaman Beranda</h1>`;
   },
   async afterRender() {
     // fungsi after render
+    const restaurant = await RestaurantApiHandler.list();
+    console.log(restaurant);
   },
 };
 
