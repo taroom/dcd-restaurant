98ed902ac435e722726f2c34bc7d7876962f3f86
sultanroomcode <sultanroomcode@gmail.com>
2021-06-16 14:49:08 +0700

    removing data

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,29 +5,3 @@ import "./components/app-hero";
 import "./components/app-nav";
 import "./components/app-grid";
 import "./components/app-grid-item";
-import Data from "./data/getdata";
-
-// membuat sebuah function untuk mengambil data dan memasang ke grid
-const callDataFunction = () => {
-  const gridList = document.querySelector("app-grid");
-  const fallbackResult = (message) => {
-    gridList.renderError(message);
-  };
-
-  const renderResult = (results) => {
-    gridList.grids = results;
-  };
-
-  async function getData() {
-    try {
-      const data = await Data.getAllData();
-      renderResult(data);
-    } catch (message) {
-      fallbackResult(message);
-    }
-  }
-
-  getData();
-};
-
-callDataFunction();
