ac6afe943d199616eca0bcac2048e502637bc1ad
sultanroomcode <sultanroomcode@gmail.com>
2021-06-16 16:25:02 +0700

    add eventlistener on index

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,3 +5,14 @@ import "./components/app-hero";
 import "./components/app-nav";
 import "./components/app-grid";
 import "./components/app-grid-item";
+import App from "./views/app";
+
+const app = new App({ content: document.querySelector("#main-place") });
+
+window.addEventListener("hashchange", () => {
+  app.renderPage();
+});
+
+window.addEventListener("load", () => {
+  app.renderPage();
+});
