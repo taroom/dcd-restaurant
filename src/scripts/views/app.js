86c78c1d574afbdf33d30092dccc42eb37b2454b
sultanroomcode <sultanroomcode@gmail.com>
2021-06-16 16:25:15 +0700

    add url parser on app

--- src/scripts/views/app.js
+++ src/scripts/views/app.js
@@ -1,20 +1,20 @@
-import DrawerInitiator from "../utils/drawer-initiator";
+import routes from "../routes/routes";
+import UrlParser from "../routes/url-parser";
 
 class App {
-  constructor({ button, drawer, content }) {
-    this._button = button;
-    this._drawer = drawer;
+  constructor({ content }) {
     this._content = content;
 
     this._initalAppShell();
   }
 
-  _initalAppShell() {
-    DrawerInitiator.init({
-      button: this._button,
-      drawer: this._drawer,
-      content: this._content,
-    });
+  _initalAppShell() {}
+
+  async renderPage() {
+    const url = UrlParser.parseActiveUrlWithCombiner();
+    const page = routes[url];
+    this._content.innerHTML = await page.render();
+    await page.afterRender();
   }
 }
 
